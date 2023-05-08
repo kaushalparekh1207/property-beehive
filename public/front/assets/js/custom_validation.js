d493cc7900c0a698743f47fdcd18651a1df1e38e
yash patel <yashpatel0197@gmail.com>
2023-05-08 18:44:56 +0530

    change pass

--- public/front/assets/js/custom_validation.js
+++ public/front/assets/js/custom_validation.js
@@ -1,4 +1,5 @@
-$('#create_an_account').click(function (){
+//Sign_up form validation
+$('#create_an_account').click(function () {
     $('#myForm').validate({
         rules: {
             regsiter_as: {
@@ -14,7 +15,7 @@ $('#create_an_account').click(function (){
             },
             confirmed_password: {
                 required: true,
-                equalTo:"#password"
+                equalTo: "#password"
             },
         },
         messages: {
@@ -30,6 +31,53 @@ $('#create_an_account').click(function (){
                 minlength: "Your password must be at least 8 characters long"
             },
             confirmed_password: {
+                required: "Confirm Password Required",
+                equalTo: "Password Is Not Matched",
+            }
+            // confirmed_password: "Please accept our terms"
+        },
+        // errorClass: "text-danger",
+        errorElement: 'small',
+        errorPlacement: function (error, element) {
+            var element_name = $(element).attr("name");
+            error.appendTo($("#" + element_name + "_error"));
+        },
+        // errorElement: 'span',
+        // errorPlacement: function (error, element) {
+        //     // error.addClass('invalid-feedback');
+        //     element.closest('#contact_check').append(error);
+        //     // $('#contact_check').append(error);
+        // },
+    });
+});
+
+//password change form validation error
+
+$('#PasswordChange').click(function () {
+    $('#myForm2').validate({
+        rules: {
+            current_password: {
+                required: true,
+            },
+
+            new_password: {
+                required: true,
+                minlength: 8
+            },
+            new_password_confirmation: {
+                required: true,
+                equalTo: "#new_password"
+            },
+        },
+        messages: {
+            register_as: {
+                required: "Please Enter your Old password"
+            },
+            new_password: {
+                required: "Please provide a New password",
+                minlength: "Your password must be at least 8 characters long"
+            },
+            new_password_confirmation: {
                 required: "Confirm Password Required",
                 equalTo: "Password Is Not Matched",
             }

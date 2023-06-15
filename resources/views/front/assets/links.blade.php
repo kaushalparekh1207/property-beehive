038bbf8cb9498eeba13a028befd58ad24a22ba27
Kaushal Parekh <kkaushal8511@gmail.com>
2023-06-15 15:35:25 +0530

    update datatable cdn & remove old pagination css

--- resources/views/front/assets/links.blade.php
+++ resources/views/front/assets/links.blade.php
@@ -1,6 +1,6 @@
 <!-- Custom CSS -->
-<link href="{{url('/')}}/front/assets/css/styles.css" rel="stylesheet">
-<link href="{{url('/')}}/front/assets/css/select2.css" rel="stylesheet">
+<link href="{{ url('/') }}/front/assets/css/styles.css" rel="stylesheet">
+<link href="{{ url('/') }}/front/assets/css/select2.css" rel="stylesheet">
 
 <!-- Select2 CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css">
@@ -9,3 +9,7 @@
 <link rel="stylesheet" href="{{ url('/') }}/plugins/summernote/summernote-bs4.min.css">
 <!-- jQuery Datepicker -->
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+
+<!-- jQuery Datatable -->
+{{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css"> --}}
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap4.min.css">

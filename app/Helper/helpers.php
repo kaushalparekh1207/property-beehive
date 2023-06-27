a0051afa135c78ed644ed711bff4af7cbce85733
Kaushal Parekh <kkaushal8511@gmail.com>
2023-06-27 12:05:59 +0530

    Add Convert Currency Function

--- app/Helper/helpers.php
+++ app/Helper/helpers.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Models\City;
-use App\Models\PropertyCategory;
 use App\Models\PropertyMaster;
 use App\Models\PropertyType;
 use App\Models\Taluka;
@@ -24,8 +23,58 @@ function taluka()
     return $taluka;
 }
 
-function footer(){
+function footer()
+{
     $allDetails = PropertyMaster::where('flag', 1)->orderBy('id', 'DESC')->limit(2)->get();
     return $allDetails;
 }
 
+function convertCurrency($number)
+{
+    $unitArray = ['4' => 'K', '5' => 'K', '6' => 'Lacs', '7' => 'Lacs', '8' => 'Cr', '9' => 'Cr'];
+    $numLen = strlen($number);
+    if ($numLen > 5) {
+        foreach ($unitArray as $nmLen => $unit) {
+            if ($numLen == $nmLen) {
+                if ($nmLen % 2 == 0) {
+                    // echo 123;
+                    // exit();
+                    $remNumber = substr($number, 1, 2);
+                    // $ckNmGtZer = $remNumber[0] + $remNumber[1] + $remNumber[2];
+                    // if ($ckNmGtZer < 1) {
+                    //     $remNumber = '';
+                    // } else {
+                    if ($remNumber[0] == 0 && $remNumber[1] == 0 && is_array($remNumber)) {
+                        $remNumber[1] = '';
+                        $remNumber[2] = '';
+                    }
+                    // }
+                    if ($remNumber == 00) {
+                        echo $number = substr($number, 0, $numLen - $nmLen + 1) . " $unit";
+                    } else {
+                        echo $number = substr($number, 0, $numLen - $nmLen + 1) . '.' . $remNumber . " $unit";
+                    }
+                } else {
+                    $remNumber = substr($number, 2, 2);
+                    // echo $remNumber;
+                    // exit();
+                    // $ckNmGtZer = $remNumber[0] + $remNumber[1] + $remNumber[2];
+                    // if ($ckNmGtZer < 1) {
+                    // } else {
+                    if ($remNumber[0] == 0 && $remNumber[1] == 0 && is_array($remNumber)) {
+                        $remNumber[1] = '';
+                        $remNumber[2] = '';
+                    }
+                    // }
+                    if ($remNumber == 00) {
+                        echo $number = substr($number, 0, $numLen - $nmLen + 2) . " $unit";
+                    } else {
+                        echo $number = substr($number, 0, $numLen - $nmLen + 2) . '.' . $remNumber . " $unit";
+                    }
+                }
+            }
+        }
+    } else {
+        echo $number . 'Rs';
+    }
+}
